b93baccfd7836e1ee96e361b58db35d5dc3d5196
archientist <archientist@gmail.com>
2015-11-30 23:04:16 +0900

    move amd app and media sdk

--- @artifact/sdk/AMD_Media_SDK/1.1/samples/mediaFoundation/videoEditAmpWinStore/ampTransform/src/CopyRGshader.hlsl
+++
@@ -1,8 +0,0 @@
-Texture2D<float2> inTexture;
-RWTexture2D<float2> outTexture;
-
-[numthreads(2,2,1)]
-void main( uint3 DTid: SV_DispatchThreadID )
-{
-    outTexture[ DTid.xy ] = inTexture[ DTid.xy ];
-}
--- @artifact/sdk/AMD_Media_SDK/1.1/samples/mediaFoundation/videoEditAmpWinStore/ampTransform/src/CopyRshader.hlsl
+++
@@ -1,8 +0,0 @@
-Texture2D<float> inTexture;
-RWTexture2D<float> outTexture;
-
-[numthreads(2,2,1)]
-void main( uint3 DTid: SV_DispatchThreadID )
-{
-    outTexture[ DTid.xy ] = inTexture[ DTid.xy ];
-}
