25fb7103d88a887d99115049106736f56122bf40
archientist <archientist@gmail.com>
2016-01-04 10:24:30 +0900

    [archientist] update dk_media_source of dk_common

--- dk_common/include/dk_media_source.h
+++ dk_common/include/dk_media_source.h
@@ -36,3 +36,33 @@ extern "C" {
 #ifdef __cplusplus
 }
 #endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+	// {56C0869F-6575-4F46-8DD9-B3A9BDF39F21}
+	DEFINE_GUID(IID_IRTMPClient,
+		0x56c0869f, 0x6575, 0x4f46, 0x8d, 0xd9, 0xb3, 0xa9, 0xbd, 0xf3, 0x9f, 0x21);
+
+
+	DECLARE_INTERFACE_(IRTMPClient, IUnknown)
+	{
+		STDMETHOD(SetUrl)(BSTR url) PURE;
+		STDMETHOD(SetUsername)(BSTR username) PURE;
+		STDMETHOD(SetPassword)(BSTR password) PURE;
+		STDMETHOD(SetRecvOption)(USHORT option) PURE;
+		STDMETHOD(SetRecvTimeout)(ULONGLONG timeout) PURE;
+		STDMETHOD(SetConnectionTimeout)(ULONGLONG timeout) PURE;
+		STDMETHOD(SetRepeat)(BOOL repeat) PURE;
+		STDMETHOD(GetUrl)(BSTR * url) PURE;
+		STDMETHOD(GetUsername)(BSTR * username) PURE;
+		STDMETHOD(GetPassword)(BSTR * password) PURE;
+		STDMETHOD(GetRecvOption)(USHORT & option) PURE;
+		STDMETHOD(GetRecvTimeout)(ULONGLONG & timeout) PURE;
+		STDMETHOD(GetConnectionTimeout)(ULONGLONG & timeout) PURE;
+		STDMETHOD(GetRepeat)(BOOL & repeat) PURE;
+	};
+
+#ifdef __cplusplus
+}
+#endif
